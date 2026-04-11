1778a3474491f8fa14a9b6e814c6f2074764c5df
Universo Merchan <universomerchan@MacBook-Air-de-Universo.local>
2026-04-11 19:50:07 +0200

    chore: force accept data loss on drizzle push deploy

--- deploy-ssh2.js
+++ deploy-ssh2.js
@@ -3,7 +3,7 @@ const { Client } = require('ssh2');
 const conn = new Client();
 conn.on('ready', () => {
     console.log('Connected to server. Executing deploy commands...');
-    conn.exec('cd /var/www/universomerchan && git fetch --all && git reset --hard origin/main && npm install && yes | npx drizzle-kit push && npm run build && pm2 restart all', (err, stream) => {
+    conn.exec('cd /var/www/universomerchan && git fetch --all && git reset --hard origin/main && npm install && npx drizzle-kit push --force --accept-data-loss && npm run build && pm2 restart all', (err, stream) => {
         if (err) throw err;
         stream.on('close', (code, signal) => {
             console.log('Deploy finished. Exit Code: ' + code);
